26c5b3b857630f948bf4ac5bd886ee2fe8a6ab90
Nguyen Anh Kha (Eric) Tran <83027539+riczfe@users.noreply.github.com>
2023-09-13 12:36:28 +0700

    #1: FIRST ATTEMPT

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,11 +1,24 @@
-public abstract class Vehicle extends Container {
+public abstract class Vehicle {
     private String registrationNumber;
     private int capacity;
+    private String id;
+    private double weight;
+    
+    private String destinationPortId;
+    
+    public String getDestinationPortId() {
+        return destinationPortId;
+    }
+    
+    public void setDestinationPortId(String destinationPortId) {
+        this.destinationPortId = destinationPortId;
+    }
 
     public Vehicle(String registrationNumber, int capacity, String id, double weight) {
-        super(id, weight);
         this.registrationNumber = registrationNumber;
         this.capacity = capacity;
+        this.id = id;
+        this.weight = weight;
     }
 
     public String getRegistrationNumber() {
@@ -24,8 +37,23 @@ public abstract class Vehicle extends Container {
         this.capacity = capacity;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
     public abstract void loadContainer(Container container);
 
     public abstract void unloadContainer(Container container);
-
 }
